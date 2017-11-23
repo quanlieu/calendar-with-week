46e970f3b54143c4f57ff900312edcf14af631a2
Quan Lieu <lieuchiquan@gmail.com>
2017-11-23 12:04:46 +0700

    Remove sinon, make some simple test

--- test/App.test.js
+++ test/App.test.js
@@ -1,18 +1,45 @@
-import { expect } from 'chai';
 import React from 'react';
-import sinon from 'sinon';
 import Enzyme from 'enzyme';
 import { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import App from '../src/app';
+import App from '../src/App';
 import Calendar from '../src/components/Calendar';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  it('renders child tag', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find(Calendar)).to.have.length(1);
+  test('should renders calendar', () => {
+    const wrapper = shallow(<App />);
+    const { year, mode } = wrapper.state();
+    const calendarProps = wrapper.find(Calendar).props();
+    expect(wrapper.find(Calendar)).toHaveLength(1);
+    expect(calendarProps.year).toBe(year);
+    expect(calendarProps.mode).toBe(mode);
+  });
+
+  test('should increase and decrease year', () => {
+    // React simulate click example
+    const spyNext = jest.spyOn(App.prototype, 'handleGoNextClick');
+    const spyBack = jest.spyOn(App.prototype, 'handleGoBackClick');
+    const wrapper = shallow(<App />);
+    const thisYear = wrapper.state().year;
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.state().year).toEqual(thisYear - 1);
+    wrapper.find('button').at(1).simulate('click');
+    expect(wrapper.state().year).toEqual(thisYear);
+    expect(spyNext).toHaveBeenCalledTimes(1)
+    expect(spyNext).toHaveBeenCalledTimes(1)
+  });
+
+  test('should change calendar mode', () => {
+    const wrapper = shallow(<App />);
+    // React component method test example
+    wrapper.instance().handleChangeMode({
+      currentTarget: { value: 'Inclusive' },
+      target: { value: 'Inclusive' }
+    });
+    expect(wrapper.state().mode).toEqual('Inclusive');
   });
 });
